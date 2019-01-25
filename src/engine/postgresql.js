b163bef1a6c73f3daddd5602c0d0f11f9c0d93f3
aawfrancois <aawfrancois@gmail.com>
2019-01-25 10:37:18 +0100

    postgressql.js

--- src/engine/postgresql.js
+++ src/engine/postgresql.js
@@ -1,3 +1,26 @@
-export default class PostgreSQL {
+import {client} from 'pg'
 
+export default class PostgreSQL {
+    constructor(options) {
+        super(options)
+    }
+
+    async initialize() {
+        const {host, port, username, password, database} = this
+
+        this.client = new Client({
+            user: username,
+            host,
+            database,
+            password,
+            port
+        })
+
+        try {
+            await this.client.connect()
+        } catch (ex) {
+            throw new Error(`Databse ${host} doesn't exist`)
+
+        }
+    }
 }
