43b4f6ba3c173322bc3483d7239fc727f8d404af
Unknown <julien.luccioni@protonmail.com>
2019-01-25 11:04:46 +0100

    other poush

--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
-import mOrm from './mOrm'
-import Student from './entities/student'
+import mOrm from "./mOrm";
+import Student from "./entities/student";
 
-const orm = new mOrm()
+const orm = new mOrm();
 
-;(async () => {
-    let connection
+(async () => {
+  let connection;
 
-    try{
-        await orm.createConnection({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username:'julienluccioni',
-            password: '',
-            database: 'test',
-            synchronize: true,
-            entities: [Student]
-        })
-    } catch (err) {
-        throw new Error(`Error Message ${err.message}`)
-        process.exit(-1)
-    }
+  try {
+    await orm.createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "julienluccioni",
+      password: "",
+      database: "test",
+      synchronize: true,
+      entities: [Student]
+    });
+  } catch (err) {
+    throw new Error(`Error Message ${err.message}`);
+    process.exit(-1);
+  }
 
-    let Student = {
-        firstname: 'Julien',
-        lastname: 'Luccioni'
-    }
+  let Student = {
+    firstname: "Julien",
+    lastname: "Luccioni"
+  };
 
-    const StudentEntity = orm.getEntity('Student')
-    console.log(StudentEntity)
-    const savedStudent = await StudentEntity.save(Student)
-})
+  const StudentEntity = orm.getEntity("Student");
+  console.log(StudentEntity);
+  const savedStudent = await StudentEntity.save(Student);
+})();
